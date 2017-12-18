d8bf1e44d6959adf9ae8dee38839a9d83f4a7fee
Ivan Yakubiv <Black.Wolfman.7@gmail.com>
2017-12-18 16:32:11 +0200

    Base GUI mockup [Finished]

--- WolfmanTaskManager/Views/TaskListPage.xaml.cs
+++ WolfmanTaskManager/Views/TaskListPage.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace WolfmanTaskManager
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Tasks.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TaskListPage : Page
     {
-        public MainWindow()
+        public TaskListPage()
         {
             InitializeComponent();
         }
