77f60194096e644744fb934805e5d09f87d8ec65
Ivan Yakubiv <Black.Wolfman.7@gmail.com>
2018-01-05 11:53:51 +0200

    Unhandled exceptions dispatcher [Created]

--- WolfmanTaskManager/App.xaml.cs
+++ WolfmanTaskManager/App.xaml.cs
@@ -19,5 +19,11 @@ namespace WolfmanTaskManager
 
             wnd.Show();
         }
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            e.Handled = true;
+        }
     }
 }
